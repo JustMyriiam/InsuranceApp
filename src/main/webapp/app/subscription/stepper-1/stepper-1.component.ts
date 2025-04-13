21bbce706a828d28ad8621aa6c1e3a37bbd6d1dd
JustMyriiam <mariemsghaier77@gmail.com>
2025-04-13 21:51:44 +0100

    second commit

---
+++ src/main/webapp/app/subscription/stepper-1/stepper-1.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-stepper-1',
+  imports: [],
+  templateUrl: './stepper-1.component.html',
+  styleUrl: './stepper-1.component.scss',
+})
+export class Stepper1Component {}
